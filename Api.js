4eb9ab7f05e3bc96ed79f620123bccf5af23866a
Cihat Kösem <44941726+CihatKsm@users.noreply.github.com>
2022-03-29 23:38:16 +0300

    :/

--- Api.js
+++ Api.js
@@ -24,19 +24,19 @@ module.exports.drizzly = {
         }
         return res.json(data)
     },
-    users: async function apifunc11(req, res, next) {
+    users: async function apifunc2(req, res, next) {
         let users = await Models.user.find()
         let token = req.params.token ? await Models.verification.findOne({ verification: req.params.token }) : null
         let data = {
             status: "OK",
             url: req.url,
             token: req.params.token ? { get: req.params.token, status: token.current == "true" ? "authorized" : "expired" } : "not written",
-            data: token ? token.current == "true" ? users : "expired" : "not written"
+            data: token ? token.current == "true" ? users ? users : "error" : "expired" : "not written"
         }
         return res.json(data)
     }
 }
-module.exports.discord = async function apifunc2(req, res, next) {
+module.exports.discord = async function apifunc3(req, res, next) {
     let user = req.params.id ? isNaN(req.params.id) !== true ? await Functions.Find.discord.user.info(req.params.id) : null : null
     let token = req.params.token ? await Models.verification.findOne({ verification: req.params.token }) : null
     let data = {
@@ -50,7 +50,7 @@ module.exports.discord = async function apifunc2(req, res, next) {
     }
     return res.json(data)
 }
-module.exports.twitter = async function apifunc3(req, res, next) {
+module.exports.twitter = async function apifunc4(req, res, next) {
     let user = req.params.name ? await Functions.Find.twitter.user(req.params.name) : null
     let token = req.params.token ? await Models.verification.findOne({ verification: req.params.token }) : null
     let data = {
@@ -64,7 +64,7 @@ module.exports.twitter = async function apifunc3(req, res, next) {
     }
     return res.json(data)
 }
-module.exports.weather = async function apifunc4(req, res, next) {
+module.exports.weather = async function apifunc5(req, res, next) {
     let status = req.params.location ? await Functions.Find.weather(req.params.location) : null
     let token = req.params.token ? await Models.verification.findOne({ verification: req.params.token }) : null
     let data = {
@@ -80,7 +80,7 @@ module.exports.weather = async function apifunc4(req, res, next) {
 
 }
 
-module.exports.error = async function apifunc5(req, res, next) {
+module.exports.error = async function apifunc6(req, res, next) {
     let data = {
         status: "OK",
         url: {

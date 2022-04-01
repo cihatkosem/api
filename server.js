b78afa9158ae6f06c34740e7bfa6b95edcf92dc8
Cihat Kösem <44941726+CihatKsm@users.noreply.github.com>
2022-04-01 09:04:36 +0300

    official site limit removed

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const rateLimit = require("express-rate-limit")
 const Functions = require("./Functions")
 const cookie_session = { cookie: { secure: true, maxAge: 60000 }, secret: "~drizzlydeveloper", resave: false, saveUninitialized: true }
 const message = { status: "Api limit exceeded", url: { url: null, args: null }, token: null, data: null }
+let drizzlydeveloper = "drizzlydeveloper.xyz"
 
-app.use(rateLimit({ windowMs: 1000, max: 3, message }))
+app.use(function api(req, res) { if(req.hostname !== drizzlydeveloper) rateLimit({ windowMs: 1000, max: 3, message }) })
 app.use(require("body-parser").urlencoded({ extended: true }))
 app.use(require("body-parser").json())
 app.use(require("cookie-session")(cookie_session))
